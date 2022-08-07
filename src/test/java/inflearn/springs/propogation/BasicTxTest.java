f96f31ee09880fca54cf0fb325d49f47766e7245
Jaesuk <jaesuk95@outlook.com>
2022-08-07 17:42:41 +0900

    기본 트랜잭션 테스트

---
+++ src/test/java/inflearn/springs/propogation/BasicTxTest.java
@@ -0,0 +1,54 @@
+package inflearn.springs.propogation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * 기본적으로 트랜잭션이 돌아가는 시험
+ * */
+@Slf4j
+@SpringBootTest
+public class BasicTxTest {
+
+    @Autowired
+    PlatformTransactionManager txManager;
+
+    /**
+     * 직접 등록,
+     * 원래는 스프링이 자동으로 등록해지만
+     * 이렇게 직접 등록해주면 이게 등록이 된다
+     * */
+    @TestConfiguration
+    static class Config {
+        @Bean
+        public PlatformTransactionManager transactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+    }
+
+    @Test
+    void commit() {
+        log.info("트랜잭션 시작");
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션 커밋 시작");
+        txManager.commit(status);
+        log.info("트랜잭션 커밋 완료");
+    }
+}
+
