fbfa6b17ddd4f6e561f40a03dff25e996c8fb47a
Jaesuk <jaesuk95@outlook.com>
2022-08-15 02:09:31 +0900

    트랜잭션 예제 프로젝트 시작

---
+++ src/main/java/inflearn/springs/propagation/Member.java
@@ -0,0 +1,24 @@
+package inflearn.springs.propagation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+    public Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
