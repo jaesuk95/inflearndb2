4caf277ec3bb9aa81e6a471532b315b96bf99772
Jaesuk <jaesuk95@outlook.com>
2022-08-16 23:46:45 +0900

    롤백 fail

--- src/test/java/inflearn/springs/propagation/MemberServiceTest.java
+++ src/test/java/inflearn/springs/propagation/MemberServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.rmi.UnexpectedException;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -97,5 +99,46 @@ class MemberServiceTest {
         // WHEN : 모든 데이터가 정상 저장된다
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
+    }
+
+
+    /**
+     * memberService        @Transactional:ON
+     * memberRepository     @Transactional:ON
+     * logRepository        @Transactional:ON EXCEPTION
+     * */
+    @Test
+    void outerTxOn_fail() {
+        // GIVEN
+        String username = "로그예외outerTxOn_success";
+
+        // WHEN
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+//        memberService.joinV1(username);
+
+        // WHEN : 모든 데이터가 롤백
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+
+    /**
+     * memberService        @Transactional:ON
+     * memberRepository     @Transactional:ON
+     * logRepository        @Transactional:ON EXCEPTION
+     * */
+    @Test
+    void recoverException_fail() {
+        // GIVEN
+        String username = "로그예외_recoverException_fail";
+
+        // WHEN
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedException.class);
+
+        // WHEN : 모든 데이터가 롤백
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
     }
 }
